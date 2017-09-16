297072139bbcbd04acc93314cc68cae0b1d654db
przemo <curzytekp@gmail.com>
2017-09-16 12:03:31 -0400

    lesson 1 exercises

--- Lesson01-Favorite-Toys/T01.03-Exercise-AddScrolling/app/src/main/java/com/example/android/favoritetoys/MainActivity.kt
+++ Lesson01-Favorite-Toys/T01.03-Exercise-AddScrolling/app/src/main/java/com/example/android/favoritetoys/MainActivity.kt
@@ -13,26 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.favoritetoys;
+package com.example.android.favoritetoys
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
 
-    private TextView mToysListTextView;
+    private var mToysListTextView: TextView? = null
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         /*
          * Using findViewById, we get a reference to our TextView from xml. This allows us to
          * do things like set the text of the TextView.
          */
-        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+        mToysListTextView = findViewById(R.id.tv_toy_names) as TextView
 
         /*
          * This String array contains names of classic toys. After all, these are toy apps. We
@@ -43,15 +42,15 @@ public class MainActivity extends AppCompatActivity {
          * concepts in Sunshine! Let us know what you think! We're really excited to have you
          * taking this course.
          */
-        String[] toyNames = ToyBox.getToyNames();
+        val toyNames = ToyBox.toyNames
 
         /*
          * Iterate through the array and append the Strings to the TextView. The reason why we add
          * the "\n\n\n" after the String is to give visual separation between each String in the
          * TextView. Later, we'll learn about a better way to display lists of data.
          */
-        for (String toyName : toyNames) {
-            mToysListTextView.append(toyName + "\n\n\n");
+        for (toyName in toyNames) {
+            mToysListTextView!!.append(toyName + "\n\n\n")
         }
     }
 }
